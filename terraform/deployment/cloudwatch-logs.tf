c5a4dd0618a19808a35aa55786ec08ce326c0981
Yaroslav Tanchevsky <jarik340@gmail.com>
2025-02-09 23:56:06 +0200

    Fix elb tg connect to ecs cluster 5

--- terraform/deployment/cloudwatch-logs.tf
+++ terraform/deployment/cloudwatch-logs.tf
@@ -5,12 +5,3 @@ resource "aws_cloudwatch_log_group" "log-group" {
     Environment = var.tag-name
   }
 }
-
-
-resource "aws_s3_bucket" "alb_logs" {
-  bucket = "${var.app_name}-alb-logs"
-
-  tags = {
-    Environment = var.tag-name
-  }
-}
